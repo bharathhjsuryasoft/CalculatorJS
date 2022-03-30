d6af82ec5920e2ca0aa38932b5bbb55460df0679
bharath <bharath.hj@surya-soft.com>
2022-03-30 17:38:58 +0530

    Comments Added
    * Comments were added
    * Infinity Error was fixed

--- js/app.js
+++ js/app.js
@@ -1,15 +1,17 @@
-let output = document.getElementById("outputContainer")
-
+// Input : the first display line on the screen
 let input = document.getElementById("input")
+
+// Input : the second display line on the screen
+let output = document.getElementById("outputContainer")
 
 function display(num){
 
     if(input.value.length > 6){
-        document.getElementById("input").style.fontSize = "48px";
+        input.style.fontSize = "48px";
     }
     
     if(input.value.length > 12){
-        document.getElementById("input").style.fontSize = "28px";
+        input.style.fontSize = "28px";
     }
     
     if(input.value.length > 20){
@@ -17,32 +19,56 @@ function display(num){
     }
 
     input.value += num;
+ 
     console.log(input.value.length); 
     
 }
 
-function clear(){
+// Clear function called in AC button which clears the screen
+function Clear(){
     input.value = "";
-    document.getElementById("outputContainer").innerText = "";
+    output.innerText = "";
 }
 
+// Delete function called in <x| button which deletes the last enterd character on the screen
 function del(){
     input.value = input.value.slice(0, -1);
 }
 
+// Calculation Function 
 function calculate(){
     try{
+
+        /* 
+           eval function Converts a string to an integer evaluates it 
+           and provides the answer that is stored in outputValue which 
+           is of type number and if any error occurs in this eval it 
+           is caught in the catch block as Invalid
+        */
         let outputValue = eval(input.value);
-        
+
         if(outputValue>999999999999){
-            document.getElementById("outputContainer").style.fontSize = "30px";
+            output.style.fontSize = "30px";
         }
 
-        document.getElementById("outputContainer").innerText = outputValue;
+        if(outputValue === Infinity){
+            output.style.fontSize = "50px";
+        }
+
+        if(outputValue === Infinity){
+            output.style.fontSize = "50px";
+            output.innerText = "Error";
+        }
+        
+        if(outputValue !== NaN){
+            console.log(outputValue);
+            console.log(typeof(outputValue));
+            output.innerText = outputValue;  
+        }
 
     }catch(err){
         input.value = "";
-        document.getElementById("outputContainer").innerText = "";
+        output.innerText = "";
         alert("Invalid") 
     }
 }
