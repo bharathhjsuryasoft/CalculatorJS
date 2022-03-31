12cf1f5cb5217d030c2fb2647410af5f39599193
bharath <bharath.hj@surya-soft.com>
2022-03-31 07:01:06 +0530

    In Calculator Web Changes made are
    * method names are changed
    * comment were changed
    * padding issue of clear button is fixed
    * NaN issue is fixed

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
-// Input : the first display line on the screen
+// Input : the first line on the screen
 let input = document.getElementById("input")
 
-// Input : the second display line on the screen
+// Output : the second line on the screen
 let output = document.getElementById("outputContainer")
 
 function display(num){
@@ -25,21 +25,20 @@ function display(num){
 }
 
 // Clear function called in AC button which clears the screen
-function Clear(){
+function clearScreen(){
     input.value = "";
     output.innerText = "";
 }
 
 // Delete function called in <x| button which deletes the last entered character on the screen
-// delete is a reserve word hence del is used as a function name
-function del(){
+function deleteCharacter(){
     input.value = input.value.slice(0, -1);
 }
 
 // Calculation Function 
 /* 
-    function evaluates return the answer that is stored in outputValue which 
-    is of type number and if any error occurs in the process it is caught in the catch block as Invalid
+    function evaluates return the answer which stored in outputValue of type number 
+    and if any error occurs in the process it is caught in the catch block as Invalid
 */
 
 function calculate(){
@@ -52,15 +51,20 @@ function calculate(){
             output.style.fontSize = "30px";
         }
 
-        if(outputValue === Infinity){
-            output.style.fontSize = "50px";
-        }
-        
-        /* TODO
-          - Here NaN comparision is not working So it should be fixed
-        */ 
-        if(outputValue != NaN){
+        // Checks whether outputValue is a number or not
+        if(!Number.isNaN(outputValue)){
             output.innerText = outputValue;  
+        }else{
+            input.value = "";
+            output.innerText = "";
+            alert("Invalid")
+        }
+
+        // Infinity Font Size
+        if(outputValue === Infinity){
+            input.value = "";
+            output.innerText = "";
+            alert("Invalid")
         }
 
     }catch(err){
